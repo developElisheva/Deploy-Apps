963a39dff0cb9fca230e1cecca5733bbf1e043dc
developElisheva <elisheva2107@gmail.com>
2025-03-04 19:51:37 +0200

    הוספת קבצי AuthClient ו-AuthServer

---
+++ AuthServer/AuthServer/Controllers/PrivateController.cs
@@ -0,0 +1,27 @@
+﻿using AuthServer.CustomAuth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AuthServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PrivateController : AuthenticatedController
+    {
+        private readonly DataContext _dataContext;
+
+        public PrivateController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Session>> Get()
+        {
+            return Ok(_dataContext.Sessions?.Where(s => s.UserId == Identity.Id).OrderByDescending(s=>s.DateTime));
+        }
+    }
+}
