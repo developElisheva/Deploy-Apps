2b989b7aa7eeb8431d4b2568aa1bc180126aa189
developElisheva <elisheva2107@gmail.com>
2025-03-11 15:33:38 +0200

    עדכון הגדרות CORS

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -9,21 +9,27 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Version = "v1" });
 });
-builder.Services.AddCors(option => option.AddPolicy("AllowAll",//נתינת שם להרשאה
-    p => p.AllowAnyOrigin()//מאפשר כל מקור
-    .AllowAnyMethod()//כל מתודה - פונקציה
-    .AllowAnyHeader()));//וכל כותרת פונקציה
+
+// הוספת קונפיגורציה של CORS עם דומיין ספציפי
+builder.Services.AddCors(option => option.AddPolicy("AllowSpecificOrigin", p =>
+{
+    p.WithOrigins("https://todolistreact-master-vs8d.onrender.com") // כתובת האתר שלך ב-React
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+}));
+
 builder.Services.AddDbContext<ToDoDbContext>(options =>
-options.UseMySql(builder.Configuration.GetConnectionString("ToDoDb"),
-    new MySqlServerVersion(new Version(8, 0, 41))));
+    options.UseMySql(builder.Configuration.GetConnectionString("ToDoDb"),
+        new MySqlServerVersion(new Version(8, 0, 41))));
 
 var app = builder.Build();
-app.UseCors("AllowAll");
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+
+// הפעלת CORS עם הפוליסי המיועד
+app.UseCors("AllowSpecificOrigin");
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapGet("/", () => "hello world");
 
 app.MapGet("/getAll", async (ToDoDbContext context) =>
@@ -31,26 +37,27 @@ app.MapGet("/getAll", async (ToDoDbContext context) =>
     return await context.Items.ToListAsync();
 });
 
-app.MapPost("/add", async (ToDoDbContext db,string Name) =>{
-    var item = new Item { Name = Name, IsComplete = false }; 
-     await db.Items.AddAsync(item);
-     await db.SaveChangesAsync();
-     return "Item added";
-} );
+app.MapPost("/add", async (ToDoDbContext db, string Name) =>
+{
+    var item = new Item { Name = Name, IsComplete = false };
+    await db.Items.AddAsync(item);
+    await db.SaveChangesAsync();
+    return "Item added";
+});
 
-app.MapPatch("/update/{id}", async (ToDoDbContext db, int id,bool IsComplete) => {
+app.MapPatch("/update/{id}", async (ToDoDbContext db, int id, bool IsComplete) =>
+{
     var existingItem = await db.Items.FindAsync(id);
-    
+
     if (existingItem == null)
     {
         return Results.NotFound();
     }
-    
-        existingItem.IsComplete = IsComplete;
-    
-    await db.SaveChangesAsync();
-    return Results.Ok("Item updated"); 
 
+    existingItem.IsComplete = IsComplete;
+
+    await db.SaveChangesAsync();
+    return Results.Ok("Item updated");
 });
 
 app.MapDelete("/deleteItem/{id}", async (ToDoDbContext context, int id) =>
