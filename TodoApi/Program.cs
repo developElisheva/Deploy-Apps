2db9266c61bd011b6f67c75b3b64f666123245cc
developElisheva <elisheva2107@gmail.com>
2025-03-12 21:24:30 +0200

    עודכן בהצלחה

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,67 +1,75 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using TodoApi;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";  
+builder.WebHost.ConfigureKestrel(options =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Version = "v1" });
+    options.ListenAnyIP(int.Parse(port)); 
 });
+
+
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("ToDoDb"), 
+    new MySqlServerVersion(new Version(8, 0, 41)),
+    mySqlOptions => mySqlOptions.EnableRetryOnFailure()));
+
+builder.Logging.AddConsole();
 builder.Services.AddCors(option => option.AddPolicy("AllowAll",//נתינת שם להרשאה
     p => p.AllowAnyOrigin()//מאפשר כל מקור
     .AllowAnyMethod()//כל מתודה - פונקציה
     .AllowAnyHeader()));//וכל כותרת פונקציה
-builder.Services.AddDbContext<ToDoDbContext>(options =>
-options.UseMySql(builder.Configuration.GetConnectionString("ToDoDb"),
-    new MySqlServerVersion(new Version(8, 0, 41))));
 
-var app = builder.Build();
-app.UseCors("AllowAll");
-// if (app.Environment.IsDevelopment())
-// {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-// }
-app.MapGet("/", () => "hello world");
-
-app.MapGet("/getAll", async (ToDoDbContext context) =>
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-    return await context.Items.ToListAsync();
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
 });
 
-app.MapPost("/add", async (ToDoDbContext db,string Name) =>{
-    var item = new Item { Name = Name, IsComplete = false }; 
-     await db.Items.AddAsync(item);
-     await db.SaveChangesAsync();
-     return "Item added";
-} );
+var app = builder.Build();
 
-app.MapPatch("/update/{id}", async (ToDoDbContext db, int id,bool IsComplete) => {
+app.UseCors("AllowAll");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "ToDoApi is running!");
+app.MapGet("/selectAll", async (ToDoDbContext db) =>
+{
+    return await db.Items.ToListAsync();
+});
+app.MapPost("/add", async (ToDoDbContext db, string Name) =>
+{
+    var item = new Item { Name = Name, IsComplete = false };
+    await db.Items.AddAsync(item);
+    await db.SaveChangesAsync();
+    return "Item added";
+});
+
+app.MapPatch("/update/{id}", async (ToDoDbContext db, int id, [FromBody] bool IsComplete) =>
+{
     var existingItem = await db.Items.FindAsync(id);
-    
     if (existingItem == null)
     {
         return Results.NotFound();
     }
-    
-        existingItem.IsComplete = IsComplete;
-    
+    existingItem.IsComplete = IsComplete;
     await db.SaveChangesAsync();
-    return Results.Ok("Item updated"); 
-
+    return Results.Ok("Item updated");
 });
 
-app.MapDelete("/deleteItem/{id}", async (ToDoDbContext context, int id) =>
+app.MapDelete("/delete/{id}", async (ToDoDbContext db, int id) =>
 {
-    var existingItem = await context.Items.FindAsync(id);
-    if (existingItem != null)
+    var itemToDelete = await db.Items.FindAsync(id);
+    if (itemToDelete == null)
     {
-        context.Items.Remove(existingItem);
-        await context.SaveChangesAsync();
+        return Results.NotFound();
     }
-    return existingItem;
+    db.Items.Remove(itemToDelete);
+    await db.SaveChangesAsync();
+    return Results.Ok("Item deleted");
 });
 
 app.Run();
--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea4d50816daded9b685ebe50ab92d197fb3e2654")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0014df561be84f86d50d2ff9c53339aea8443553")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
