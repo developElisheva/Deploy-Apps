f496d9a94445d551a61d6d17b6457e5f3baf0734
developElisheva <elisheva2107@gmail.com>
2025-03-12 17:03:52 +0200

    הוספת קבצים חדשים

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e3827eea57b4a8599e06097ed8f2f618b813293")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a73ff872402d91db399235f95a488b75053cdecd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
