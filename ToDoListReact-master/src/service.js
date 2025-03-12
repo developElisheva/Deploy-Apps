b80c941d0fd7c034eb47aa3691a9fceccba4653a
developElisheva <elisheva2107@gmail.com>
2025-03-12 20:46:09 +0200

    הוספת קבצים חדשים

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -17,7 +17,7 @@ export default {
   addTask: async (name) => {
     console.log('addTask', name)
     try {
-      const result = await axios.post(`add?Name=${encodeURIComponent(name)}`);
+      const result = await axios.post(`add?Name=${encodeURIComponent(name)}`)||[];
       return result.data;
     } catch (err) {
       console.error('Error adding task:', err);
