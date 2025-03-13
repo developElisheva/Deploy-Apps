04d955a97566fbce87674ec95b4bc81fcda3f788
developElisheva <elisheva2107@gmail.com>
2025-03-13 17:34:54 +0200

    עודכן

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://todoapi-2l8v.onrender.com/';
-console.log( API_BASE_URL);
+// בסביבת פיתוח
+const API_BASE_URL = process.env.NODE_ENV === 'development' 
+  ? 'https://todoapi-2l8v.onrender.com'
+  : '';console.log( API_BASE_URL);
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL
