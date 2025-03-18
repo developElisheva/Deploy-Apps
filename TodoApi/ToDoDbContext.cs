a9296578541ea67100612f754c6dca78d8f358a0
developElisheva <elisheva2107@gmail.com>
2025-03-18 15:48:25 +0200

    change

--- TodoApi/ToDoDbContext.cs
+++ TodoApi/ToDoDbContext.cs
@@ -22,8 +22,7 @@ public partial class ToDoDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=ToDoDB;user=root;password=eli@2107", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
-        // => optionsBuilder.UseMySql("server=btvqhj0fvvfga3aibazw-mysql.services.clever-cloud.com;port=3306;user=uo3ow8vbc7blfkf3;password=2u4UKfA8FmRVVvwQy8qh;database=btvqhj0fvvfga3aibazw;", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
+        => optionsBuilder.UseMySql("server=btvqhj0fvvfga3aibazw-mysql.services.clever-cloud.com;port=3306;user=uo3ow8vbc7blfkf3;password=2u4UKfA8FmRVVvwQy8qh;database=btvqhj0fvvfga3aibazw;", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
